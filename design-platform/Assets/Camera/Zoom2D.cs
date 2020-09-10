60deb068354125afb48a048a4379292ae1f5638f
Sebastiandmeier <sebastiandmeier@gmail.com>
2020-09-10 12:24:11 +0200

    3D zoom

---
+++ design-platform/Assets/Camera/Zoom2D.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zoom2D : MonoBehaviour
+{
+    float scroll;
+    float size;
+    public float speed;
+    void Update()
+    {
+        scroll = Input.GetAxis("Mouse ScrollWheel");
+
+        if (scroll != 0)
+        {
+            size = GetComponent<Camera>().orthographicSize;
+            
+            if (scroll > 0 && size >= 5.0)
+            {
+                GetComponent<Camera>().orthographicSize = GetComponent<Camera>().orthographicSize - speed;
+            }
+            
+            if (scroll < 0 && size <= 200.0)
+            {
+                GetComponent<Camera>().orthographicSize = GetComponent<Camera>().orthographicSize + speed;
+            }
+        }
+    }
+}
