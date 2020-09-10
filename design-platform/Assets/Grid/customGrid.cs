d57f2af17d3ba4a799c55c5b53e82bd741701e26
Sebastiandmeier <sebastiandmeier@gmail.com>
2020-09-10 11:22:12 +0200

    gnome grid

---
+++ design-platform/Assets/Grid/customGrid.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class customGrid : MonoBehaviour
+{
+    public GameObject placedObject;
+    public float gridSize;
+
+    Vector3 truePos;
+
+    void LateUpdate()
+    {
+        truePos.x = Mathf.Floor(placedObject.transform.position.x / gridSize) * gridSize;
+        truePos.y = Mathf.Floor(placedObject.transform.position.y / gridSize) * gridSize;
+        truePos.z = Mathf.Floor(placedObject.transform.position.z / gridSize) * gridSize;
+
+        placedObject.transform.position = truePos;
+    }
+}
