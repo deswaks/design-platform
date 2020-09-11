65ab7c686f136ee18e48c2dae878dd5d1ac12539
Sebastiandmeier <sebastiandmeier@gmail.com>
2020-09-11 12:42:11 +0200

    add buttons

---
+++ design-platform/Assets/Sebastian/UI/buttonQuit.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class buttonQuit : MonoBehaviour
+{
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(ButtonFunction);
+    }
+
+    private void ButtonFunction()
+    {
+        Application.Quit();
+    }
+}
