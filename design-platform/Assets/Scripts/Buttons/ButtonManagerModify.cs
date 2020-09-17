78e7ef1d751f991f5afa505249f70990437e2fce
Sebastiandmeier <sebastiandmeier@gmail.com>
2020-09-17 12:23:45 +0200

    Add button managers

---
+++ design-platform/Assets/Scripts/Buttons/ButtonManagerModify.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonManagerModify : MonoBehaviour
+{
+    public void Move()
+    {
+        Debug.Log("Move function is not implemented");
+    }
+    public void Rotate()
+    {
+        Debug.Log("Rotate function is not implemented");
+    }
+    public void Modify()
+    {
+        Debug.Log("Modify function is not implemented");
+    }
+    public void Properties()
+    {
+        Debug.Log("Properties function is not implemented");
+    }
+    public void Delete()
+    {
+        Debug.Log("Delete function is not implemented");
+    }
+}
