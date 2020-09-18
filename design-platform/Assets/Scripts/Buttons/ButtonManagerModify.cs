274a0b51ea4e2306ae448ebceba679b79e376026
Niklas Rosenkilde Rasmussen <niklas.rosenkilde@oulook.com>
2020-09-18 12:32:21 +0200

    Move function works!

--- design-platform/Assets/Scripts/Buttons/ButtonManagerModify.cs
+++ design-platform/Assets/Scripts/Buttons/ButtonManagerModify.cs
@@ -8,7 +8,7 @@ public class ButtonManagerModify : MonoBehaviour
 
     public void Move()
     {
-        Debug.Log("Move function is not implemented");
+        main.modifyMode.selectedRoom.SetIsInMoveMode(isInMoveMode: true);
     }
     public void Rotate()
     {
