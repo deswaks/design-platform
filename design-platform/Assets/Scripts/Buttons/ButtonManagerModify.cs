8c85daa26ecc6dc5ee3e74b95ba32d4f1939958f
Sebastiandmeier <sebastiandmeier@gmail.com>
2020-10-02 09:29:05 +0200

    ...

--- design-platform/Assets/Scripts/Buttons/ButtonManagerModify.cs
+++ design-platform/Assets/Scripts/Buttons/ButtonManagerModify.cs
@@ -6,21 +6,13 @@ using UnityEngine.ProBuilder;
 using UnityEngine.ProBuilder.MeshOperations;
 
 public class ButtonManagerModify : MonoBehaviour {
-    public Main main;
-    // Tilføjet ABC
-    private List<int> verticalFace;
-    private List<int> baseFace;
-    private List<Vector3> baseFaceCorners;
-    private Vector3 facePos;
-    //public GameObject moveHandlePrefab;
-    //private GameObject moveHandle;
-    //private Room prefabRoom;
 
     public void Move()
     {
         ModifyMode.Instance.SetModifyMode(ModifyMode.ModifyModeType.Move);
         //main.modifyMode.selectedRoom.SetIsInMoveMode(isInMoveMode: true);
     }
+
     public void Rotate()
     {
         ModifyMode.Instance.SetModifyMode(ModifyMode.ModifyModeType.Rotate);
@@ -29,12 +21,18 @@ public class ButtonManagerModify : MonoBehaviour {
         //    main.modifyMode.selectedRoom.Rotate();
         //}
     }
+
     public void Modify()
     {
         ModifyMode.Instance.SetModifyMode(ModifyMode.ModifyModeType.Edit);
     }
+
     public void Properties() {
-        // (Midlertidigt Sebastian) Test af structural modul
+
+        
+    }
+
+    public void StructuralAnalysis() {
         foreach (Room room in Building.Instance.GetRooms()) {
             Dictionary<int, List<Structural.Load>> loadTable = Structural.LoadDistribution.AreaLoad(room);
             foreach (int indexWall in loadTable.Keys) {
@@ -42,9 +40,9 @@ public class ButtonManagerModify : MonoBehaviour {
                     Debug.Log("Load on wall: " + indexWall + "  Start: " + load.pStart + "  End: " + load.pEnd + "  Magnitude: " + load.magnitude);
                 }
             }
-         }
-        
+        }
     }
+
     public void Delete()
     {
         ModifyMode.Instance.SetModifyMode(ModifyMode.ModifyModeType.Delete);
