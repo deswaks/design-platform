b96af433a034fcec11c643b3f7c4ee00f5cb3c89
Sebastiandmeier <sebastiandmeier@gmail.com>
2020-10-23 14:46:41 +0200

    add structural analysis panel

---
+++ design-platform/Assets/Scripts/Modules/Structural Analysis/Widget/Widget.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Structural {
+    public class Widget : global::Widget {
+
+        public Widget(GameObject panel) : base(panel) {
+            Size = (1, 1);
+
+        }
+
+        public override void Update() {
+
+        }
+    }
+}
--- design-platform/Assets/Scripts/UI/Widget.cs
+++ design-platform/Assets/Scripts/UI/Widget.cs
@@ -1,18 +1,21 @@
-﻿using System.Collections;
+﻿using Microsoft.Isam.Esent.Interop;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Widget : MonoBehaviour
+public abstract class Widget
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    public (int width, int height) Size;
+    public GameObject Panel { get; private set; }
+
+    public Widget(GameObject panel) {
+        Panel = panel;
+        GameObject prefabPanel = (GameObject)Resources.Load("Scripts/Modules/Structural Analysis/Widget/Widget");
+        Panel = GameObject.Instantiate(prefabPanel, panel.transform.position, panel.transform.rotation);
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public abstract void Update();
+
+
 }
