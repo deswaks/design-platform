179c402e410c4ad131842c03e9d7ada009bf2199
Niklas Rosenkilde Rasmussen <niklas.rosenkilde@oulook.com>
2020-10-29 11:11:52 +0100

    Dashboard class WIP

---
+++ design-platform/Assets/Scripts/UI/Dashboard.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Dashboard {
+
+
+    public class Dashboard {
+        public static Dashboard instance;
+
+        private List<Widget> widgets = new List<Widget>();
+
+        public GameObject WidgetArea;
+        public GameObject RowTemplate;
+
+        public static Dashboard Instance {
+            get { return instance ?? (instance = new Dashboard()); }
+        }
+
+        public void InsertWidgets() {
+            
+            foreach(Widget widget in widgets){
+
+
+
+
+
+
+                
+                widget.Draw(host);
+                
+            }
+        }
+
+        public void AddWidgetToList(Widget widget) {
+            widgets.Add(widget);
+        }
+
+        public void UpdateCurrentWidgets() {
+
+        }
+
+    }
+}
+
