bcf18985b47f4a2336f40fa14083948762e507b5
Sebastiandmeier <sebastiandmeier@gmail.com>
2020-10-29 10:21:24 +0100

    ...

--- design-platform/Assets/Scripts/UI/Widget.cs
+++ design-platform/Assets/Scripts/UI/Widget.cs
@@ -8,11 +8,7 @@ public abstract class Widget {
     public (int width, int height) Size;
     public GameObject PrefabPanel;
     public GameObject Panel;
-    public GameObject Host;
 
-    public Widget(GameObject host){
-        Host = host;
-    }
     public abstract void InsertInDashboard();
     public abstract GameObject DrawPanel();
     public abstract void UpdatePanel();
