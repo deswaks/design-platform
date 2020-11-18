ce418e686ad9902c219a14a067b2e82d343be8e6
Niklas Rosenkilde Rasmussen <niklas.rosenkilde@oulook.com>
2020-11-18 18:11:36 +0100

    Publish roofies

---
+++ design-platform/Assets/Scripts/BuildingElements/Roof.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+using ProceduralToolkit.Buildings;
+using ProceduralToolkit;
+using static ProceduralToolkit.Buildings.BuildingGenerator;
+using ProceduralToolkit.Skeleton;
+
+namespace DesignPlatform.Core {
+    
+    public class Roof : MonoBehaviour {
+
+        public ProceduralToolkit.RendererProperties rendererProperties;
+        public Material roofMaterial;
+        public float RoofPitch = 25;
+
+        void Start() {
+            List<Vector2> foundationPolygon = new List<Vector2> {
+                new Vector2(0, 0),
+                new Vector2(-2, 0),
+                new Vector2(-2, 3),
+                new Vector2(5, 3),
+                new Vector2(5, 0),
+                new Vector2(3, 0),
+                new Vector2(3, -3),
+                new Vector2(0, -3),
+            };
+
+            Config config = new Config();
+            config.roofConfig.thickness = 0.01f;
+            config.roofConfig.overhang = 0.0f;
+            config.roofConfig.type = RoofType.Gabled;
+
+            // ROOF CREATION ////////////////////////////
+            ProceduralRoofPlanner roofPlanner = new ProceduralRoofPlanner();
+
+            IConstructible<MeshDraft> constructible = roofPlanner.Plan(foundationPolygon, config);
+            var draft = constructible.Construct(Vector2.zero);
+
+            var meshFilter = gameObject.AddComponent<MeshFilter>();
+            meshFilter.mesh = draft.ToMesh();
+
+            var meshRenderer = gameObject.AddComponent<MeshRenderer>();
+            meshRenderer.material = roofMaterial;
+            //////////////////////////////////////////////
+
+
+
+            // COMBINING TRIANGLES TO FACES /////////////////////////////////////
+            Mesh roofMesh = draft.ToMesh();
+            roofMesh.RecalculateNormals();
+            
+
+            // Roof mesh triangle indices and vertices. 
+            int[] allTriangleIndices = roofMesh.triangles;
+            Vector3[] vertices = roofMesh.vertices;
+            Vector3[] normals = roofMesh.normals;
+
+            // List for each individual triangle made up by vertices (a,b,c)
+            List<(Vector3 a, Vector3 b, Vector3 c)> triangleVertices = new List<(Vector3 a, Vector3 b, Vector3 c)>();
+            List<(Vector3 a, Vector3 b, Vector3 c)> triangleNormals = new List<(Vector3 a, Vector3 b, Vector3 c)>();
+
+
+            // Splits index-list into chunks of three (providing the three vertex-indices of each triangle) and adds triangles to list
+            RoofUtils.SplitList(allTriangleIndices.ToList(), 3).ForEach(t => triangleVertices.Add((vertices[t[0]], vertices[t[1]], vertices[t[2]])));
+            RoofUtils.SplitList(allTriangleIndices.ToList(), 3).ForEach(t => triangleNormals.Add((normals[t[0]], normals[t[1]], normals[t[2]])));
+
+
+            List<int> indicesToKeep = new List<int>();
+
+            // Removes faulty triangles, where two vertices have the same coordiates
+            triangleVertices.Where(t => !(Vector3.Distance(t.a, t.b) < 0.01 || Vector3.Distance(t.a, t.c) < 0.01 || Vector3.Distance(t.c, t.b) < 0.01)).ToList().ForEach(t=> indicesToKeep.Add(triangleVertices.IndexOf(t)));
+            triangleVertices = indicesToKeep.Select(j => triangleVertices[j]).ToList();
+            triangleNormals = indicesToKeep.Select(j => triangleNormals[j]).ToList();
+
+            triangleNormals = triangleNormals.Select(t =>
+            (
+                new Vector3(Mathf.Round(t.a.x * 10) / 10, Mathf.Round(t.a.y * 10) / 10, Mathf.Round(t.a.z * 10) / 10),
+                new Vector3(Mathf.Round(t.b.x * 10) / 10, Mathf.Round(t.b.y * 10) / 10, Mathf.Round(t.b.z * 10) / 10),
+                new Vector3(Mathf.Round(t.c.x * 10) / 10, Mathf.Round(t.c.y * 10) / 10, Mathf.Round(t.c.z * 10) / 10)
+            )
+            ).ToList();
+
+            Debug.Log("True for all? "+ triangleNormals.Where(t=> ( Mathf.Abs(Vector3.Angle(t.a, t.b)) < 0.01 && Mathf.Abs(Vector3.Angle(t.a, t.c)) < 0.01 && Mathf.Abs(Vector3.Angle(t.b, t.c)) < 0.01)).Count().ToString());
+            (Vector3 aaa, Vector3 bbb, Vector3 ccc) = triangleNormals
+                .Where(t => !(Mathf.Abs(Vector3.Angle(t.a, t.b)) < 0.01 && Mathf.Abs(Vector3.Angle(t.a, t.c)) < 0.01 && Mathf.Abs(Vector3.Angle(t.c, t.b)) < 0.01))
+                .ToList()[0];
+            Debug.Log("aaa: " + aaa.ToString());
+            Debug.Log("bbb: " + bbb.ToString());
+            Debug.Log("ccc: " + ccc.ToString());
+            Debug.Log("Angle? " + triangleNormals
+                .Where(t => !(Vector3.Angle(t.a, t.b) < 0.01 && Vector3.Angle(t.a, t.c) < 0.01 && Vector3.Angle(t.c, t.b) < 0.01))
+                .Select(t=> (Vector3.Angle(t.a, t.b), Vector3.Angle(t.a, t.c), Vector3.Angle(t.c, t.b)))
+                .ToList()[0]
+                .ToString()
+                );
+            //Debug.Log("Dist? " + triangleNormals
+            //    .Where(t => !(Vector3.Distance(t.a, t.b) < 0.1 && Vector3.Distance(t.a, t.c) < 0.1 && Vector3.Distance(t.c, t.b) < 0.1))
+            //    .Select(t => (Vector3.Distance(t.a, t.b), Vector3.Distance(t.a, t.c), Vector3.Distance(t.c, t.b)))
+            //    .ToList()[0]
+            //    .ToString()
+            //);
+
+            Debug.Log("Vertices amount: " + triangleVertices.Count);
+            Debug.Log("Normals amount: " + triangleNormals.Count);
+            Debug.Log("Indices amount: " + indicesToKeep.Count);
+
+
+
+            Debug.Log(triangleNormals.Count + "   " + triangleVertices.Count);
+
+            for(int i=0; i < triangleVertices.Count() ; i++) {
+
+                Debug.Log("("+triangleVertices[i].a.ToString() + ";" + triangleVertices[i].b.ToString() + ";" + triangleVertices[i].c.ToString() + ") N: (" +
+                    triangleNormals[i].a.ToString() + ";" + triangleNormals[i].b.ToString() + ";" + triangleNormals[i].c.ToString());
+
+                List<Vector3> vx = new List<Vector3> { triangleVertices[i].a, triangleVertices[i].b, triangleVertices[i].c };
+                vx.ForEach(v => {
+                    GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                    sphere.name = "gable " + i.ToString();
+                    sphere.transform.position = v;
+                    sphere.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+                });
+            }
+
+
+
+
+
+
+        }
+
+    }
+}
---
+++ design-platform/Assets/Scripts/Utilities/RoofPlanner.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ProceduralToolkit.Buildings;
+using ProceduralToolkit;
+
+namespace DesignPlatform.Core {
+    public class ProceduralRoofPlanner : RoofPlanner {
+        public override IConstructible<MeshDraft> Plan(List<Vector2> foundationPolygon, BuildingGenerator.Config config) {
+            switch (config.roofConfig.type) {
+                case RoofType.Flat:
+                    return new ProceduralFlatRoof(foundationPolygon, config.roofConfig, config.palette.roofColor);
+                case RoofType.Hipped:
+                    return new ProceduralHippedRoof(foundationPolygon, config.roofConfig, config.palette.roofColor);
+                case RoofType.Gabled:
+                    return new ProceduralGabledRoof(foundationPolygon, config.roofConfig, config.palette.roofColor);
+                default:
+                    return new ProceduralGabledRoof(foundationPolygon, config.roofConfig, config.palette.roofColor);
+            }
+        }
+    }
+    public class ProceduralRoofConstructor : RoofConstructor {
+        [SerializeField]
+        private RendererProperties rendererProperties = null;
+        [SerializeField]
+        private Material roofMaterial = null;
+
+        public override void Construct(IConstructible<MeshDraft> constructible, Transform parentTransform) {
+            var draft = constructible.Construct(Vector2.zero);
+
+            var meshFilter = parentTransform.gameObject.AddComponent<MeshFilter>();
+            meshFilter.mesh = draft.ToMesh();
+
+            var meshRenderer = parentTransform.gameObject.AddComponent<MeshRenderer>();
+            meshRenderer.ApplyProperties(rendererProperties);
+            meshRenderer.material = roofMaterial;
+        }
+    }
+
+    public static class RoofUtils {
+        public static List<List<T>> SplitList<T>(this List<T> me, int size = 50) {
+            var list = new List<List<T>>();
+            for (int i = 0; i < me.Count; i += size)
+                list.Add(me.GetRange(i, Mathf.Min(size, me.Count - i)));
+            return list;
+        }
+        public static MeshDraft ConstructGableDraft(List<Vector2> skeletonPolygon2, float roofPitch) {
+            Vector2 edgeA2 = skeletonPolygon2[0];
+            Vector2 edgeB2 = skeletonPolygon2[1];
+            Vector2 peak2 = skeletonPolygon2[2];
+            Vector2 edgeDirection2 = (edgeB2 - edgeA2).normalized;
+
+            float peakHeight = CalculateVertexHeight(peak2, edgeA2, edgeDirection2, roofPitch);
+            Vector3 edgeA3 = edgeA2.ToVector3XZ();
+            Vector3 edgeB3 = edgeB2.ToVector3XZ();
+            Vector3 peak3 = new Vector3(peak2.x, peakHeight, peak2.y);
+            Vector2 gableTop2 = Closest.PointSegment(peak2, edgeA2, edgeB2);
+            Vector3 gableTop3 = new Vector3(gableTop2.x, peakHeight, gableTop2.y);
+
+            return new MeshDraft().AddTriangle(edgeA3, edgeB3, gableTop3, true)
+                .AddTriangle(edgeA3, gableTop3, peak3, true)
+                .AddTriangle(edgeB3, peak3, gableTop3, true);
+        }
+        public static MeshDraft ConstructContourDraft(List<Vector2> skeletonPolygon2, float roofPitch) {
+            Vector2 edgeA = skeletonPolygon2[0];
+            Vector2 edgeB = skeletonPolygon2[1];
+            Vector2 edgeDirection2 = (edgeB - edgeA).normalized;
+            Vector3 roofNormal = CalculateRoofNormal(edgeDirection2, roofPitch);
+
+            var skeletonPolygon3 = skeletonPolygon2.ConvertAll(v => v.ToVector3XZ());
+
+            var tessellator = new Tessellator();
+            tessellator.AddContour(skeletonPolygon3);
+            tessellator.Tessellate(normal: Vector3.up);
+            var contourDraft = tessellator.ToMeshDraft();
+
+            for (var i = 0; i < contourDraft.vertexCount; i++) {
+                Vector2 vertex = contourDraft.vertices[i].ToVector2XZ();
+                float height = CalculateVertexHeight(vertex, edgeA, edgeDirection2, roofPitch);
+                contourDraft.vertices[i] = new Vector3(vertex.x, height, vertex.y);
+                contourDraft.normals.Add(roofNormal);
+            }
+            return contourDraft;
+        }
+        public static float CalculateVertexHeight(Vector2 vertex, Vector2 edgeA, Vector2 edgeDirection, float roofPitch) {
+            float distance = Distance.PointLine(vertex, edgeA, edgeDirection);
+            return Mathf.Tan(roofPitch * Mathf.Deg2Rad) * distance;
+        }
+
+        public static Vector3 CalculateRoofNormal(Vector2 edgeDirection2, float roofPitch) {
+            return Quaternion.AngleAxis(roofPitch, edgeDirection2.ToVector3XZ()) * Vector3.up;
+        }
+
+
+    }
+
+}
