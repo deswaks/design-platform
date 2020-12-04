56b7aa45de56b777d2825f2b5c8b5f6ce4252587
Sebastiandmeier <sebastiandmeier@gmail.com>
2020-12-04 13:18:59 +0100

    made structural widget do something instead of being empty

--- design-platform/Assets/Scripts/Modules/Structural Analysis/Widget/StructuralWidget.cs
+++ design-platform/Assets/Scripts/Modules/Structural Analysis/Widget/StructuralWidget.cs
@@ -1,6 +1,9 @@
 ﻿using DesignPlatform.UI;
 using DesignPlatform.Utils;
+using DesignPlatform.Core;
 using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace StructuralAnalysis {
 
@@ -16,13 +19,24 @@ namespace StructuralAnalysis {
         /// which is inserted into the widget afterwards.</summary>
         private readonly GameObject PrefabRow;
 
+        /// <summary>Names of used GameObjects in prefab</summary>
+        private static readonly string HeaderRowName = "Row_Headers";
+
+        /// <summary></summary>
+        private static readonly string ContentTemplateName = "Row_ContentTemplate";
+
+        /// <summary></summary>
+        private List<GameObject> addedRows = new List<GameObject>();
+
+        GameObject Table;
+
         /// <summary>
         /// Constructor for this widget class
         /// </summary>
         public StructuralWidget() : base() {
             Name = "Structural Widget";
             PrefabPanel = AssetUtil.LoadAsset<GameObject>("structuralanalysis", "Structural Widget");
-            PrefabRow = AssetUtil.LoadAsset<GameObject>("structuralanalysis", "Loads Overview Row");
+            PrefabRow = AssetUtil.LoadAsset<GameObject>("structuralanalysis", "DataRow");
         }
 
         /// <summary>
@@ -30,15 +44,65 @@ namespace StructuralAnalysis {
         /// </summary>
         /// <returns>The panel that visually represents this widget.</returns>
         public override Object CreatePanel() {
-            // This widget only uses a prefab
-            return Object.Instantiate(PrefabPanel);
+            // Loads prefab object and instantiates Widget
+            Panel = Object.Instantiate(PrefabPanel);
+
+            Table = Panel.transform.GetChild(2).gameObject;
+
+            return Panel;
         }
 
         /// <summary>
         /// Updates the gameobject that visually represents this widget
         /// </summary>
         public override void UpdatePanel() {
-            // An update method will usually go through relevant information and insert in the correct places
+
+            // Sum elements
+            float structuralWallMeters = 0;
+            float nonStructuralWallMeters = 0;
+            foreach (DesignPlatform.Core.Space space in Building.Instance.Spaces) {
+                Dictionary<int, List<DistributedLoad>> loadTable = LoadDistributor.DistributeAreaLoads(space);
+                List<int> structuralWallIndices = loadTable.Keys.ToList();
+                
+                // Sum the length of structural and non-structural wall elements
+                for (int wallIndex = 0; wallIndex < space.ControlPoints.Count; wallIndex++) {
+                    if (structuralWallIndices.Contains(wallIndex)) structuralWallMeters += space.Faces[wallIndex].LocationLine.Length;
+                    else nonStructuralWallMeters += space.Faces[wallIndex].LocationLine.Length;
+                }
+            }
+
+            // Build data rows
+            DeleteContentRows();
+
+            List<List<string>> dataRows = new List<List<string>> {
+                new List<string> {"Structural elements", structuralWallMeters.ToString() + " m"},
+                new List<string> {"Architectural elements", nonStructuralWallMeters.ToString() + " m" }
+            };
+
+            // Add data to widget
+            int elementIndex = 0;
+
+            for (int i = 0; i < dataRows.Count; i++) {
+                // Create and add row to table
+                GameObject dataRow = GameObject.Instantiate(PrefabRow, Table.transform);
+                dataRow.name = "DataRow" + elementIndex.ToString();
+                addedRows.Add(dataRow);
+
+                // Fill row with values
+                for (int j = 0; j < dataRow.transform.childCount; j++) {
+                    dataRow.transform.GetChild(j).GetComponent<TMPro.TMP_Text>().text = dataRows[i][j];
+                }
+            }
+        }
+
+        /// <summary>
+        /// Deletes the rows of information that wxists in the visual panel of this widget.
+        /// </summary>
+        private void DeleteContentRows() {
+            foreach (GameObject row in addedRows) {
+                GameObject.Destroy(row);
+            }
+            addedRows = new List<GameObject>();
         }
     }
 
